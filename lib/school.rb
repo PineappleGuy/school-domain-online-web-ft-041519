e37f1b9faf19082b0a4d101d5ef39ff0620d6602
Garth Lilly <garff247@gmail.com>
2019-04-25 04:24:50 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,29 +1,16 @@
 # code here!
 class School
-  attr_accessor :roster
-  attr_reader :name, :grade
+  attr_accessor :name, :grade
 
   def initialize(name, roster)
     @name = name
     @roster = {}
   end
 
-  def add_student(grade, name)
+  def add_student(name, grade)
     @name = name
     @grade = grade
-    roster << [:"#{grade}"=>"#{name}"]
+
   end
-
-end
-
-class Student
-  attr_reader :name, :grade
-
-  def initialize(name, grade)
-    @name = name
-    @grade = grade
-  end
-
-
 
 end
