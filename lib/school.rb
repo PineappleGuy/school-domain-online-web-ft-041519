2a5f831a91b2fc0f9024375e3e49ed6af00db46a
Garth Lilly <garff247@gmail.com>
2019-04-25 04:38:01 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -3,10 +3,19 @@ class School
   attr_accessor :name, :grade
 
 
+ROSTER = {}
   def initialize(name)
     @name = name
-    roster = Hash.new
   end
+
+  def roster=(roster)
+    @roster = roster
+  end
+
+  def roster
+    @roster
+  end
+  
 
   def add_student(name, grade)
     puts "hi"
